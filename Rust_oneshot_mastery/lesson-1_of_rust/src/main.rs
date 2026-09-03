6997f7a1282456fa22a8feb4f5e92e3bea474174
Sanskar <sanskarin@outlook.in>
2026-09-03 14:59:18 +0530

    feat(lesson-1_of_rust): add main.rs source code

---
+++ Rust_oneshot_mastery/lesson-1_of_rust/src/main.rs
@@ -0,0 +1,16 @@
+//----------------------------------------
+// See PPT:- "https://docs.google.com/presentation/d/1UnPZV7K6s798RAlkanOFn1iHDK1uMqBJTnzUKYvuT2Q/edit?slide=id.g26fdac204f3_0_892#slide=id.g26fdac204f3_0_892" for more info of this crash course.
+// Hey everyone this is the first source code file for the Rust learning from the zero to the hero level with having the real world projects and in this file we are have learned about the print function and msy the use of the comments.
+// Hey this is a comment in the Rust Language.
+fn main() {
+    println!("This is coded by the Prince and Sanskar.");
+}
+//---------------------------------------
+// The comment does not affect that what is in the code written , and also the comment is for the explaining purpose only.
+// The code ends here. (See docs/README.md for the more information).
+// ---------------------------------------
+// There are two cases one case is used in Rust and one is used in the JavaScript, you can search on any Search Engine about it for the more info.
+// about snake_case means hello_world in this way -Rust Follows
+// about CamelCase means helloWorld in this way -JavaScript Follows
+// ---------------------------------------
+// The code ends here.
