baac305c51c1de1c94fb74e3e0c4b7dd87364e5f
Sanskar <sanskarin@outlook.in>
2026-09-03 14:56:18 +0530

    feat(exercise-variables): add main.rs source code

---
+++ Rust_oneshot_mastery/Exercises/variables_in_rust/src/main.rs
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------
+// Hey everyone in this exercise we are going to learn something about the variable and data types in the Rust prgramming.
+// Let us try to understand by doing as example.
+fn main() {
+    let mut x: &str = "Sanskar";
+    println!("My name is {x}");
+    // First le change the name in the variable by using the muttable and immutable.
+    // x= Coder
+    // or we can write this in the this way:-
+    // println!("I am the {}",x)
+}
+// ---------------------------------------------------------
+// See the next exercise for the more info.
+// The code ends here.
