86af01d53bb19a2c679814334309274e2f53a888
Sanskar <sanskarin@outlook.in>
2026-09-03 14:56:12 +0530

    feat(exercise-print_your_name): add main.rs source code

---
+++ Rust_oneshot_mastery/Exercises/print_your_name_in_rust/src/main.rs
@@ -0,0 +1,8 @@
+// --------------------------------------------------
+// Hey everyone I am going to print the my name in the Rust programming language by using the println! function so, let's do it.
+ fn main() {
+    println! ("My name is Sanskar.")
+ }
+ // -------------------------------------------------
+ // And even when in the next exercise we can print the our name by creating the variable also and the variables are also of the two type:- means the muttable and the immutable variables and immutables means the variable value later can't be changed and muttable means later can be changed. See for the next next rust exercise document for the more information.
+ // The code ends here.
