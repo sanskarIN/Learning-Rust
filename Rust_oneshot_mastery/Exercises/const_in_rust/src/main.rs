a8f969bca72f70a323ad495afba1f7648e33da0b
Sanskar <sanskarin@outlook.in>
2026-09-07 20:26:13 +0530

    feat(exercise-const_in_rust): add source code

---
+++ Rust_oneshot_mastery/Exercises/const_in_rust/src/main.rs
@@ -0,0 +1,8 @@
+// Hey everyone today we are going to understand the concept of the Constant in the Rust, before we start the concept of the constant we must know some rules like the constant is always in the capital letter means it is not to be in the small letter just like the variables, like we can create the variable likely to be const-vari, cons_vari but in constant we must create like CONST-VARI, CONST_VARI this in Rust.
+// -------------------------------------------------------------------------------------------------------
+fn main() {
+    const CONST:u8 = 50;
+    // We are going to print this const.
+    println! ("The value of CONST constant is {CONST}")
+}
+// The code ends here.
