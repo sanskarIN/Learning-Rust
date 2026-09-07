0587abaedd32212969bfd3bcba090c6bd9908ece
Sanskar <sanskarin@outlook.in>
2026-09-07 20:24:15 +0530

    refactor(exercise-variables): update main.rs source code

--- Rust_oneshot_mastery/Exercises/variables_in_rust/src/main.rs
+++ Rust_oneshot_mastery/Exercises/variables_in_rust/src/main.rs
@@ -4,10 +4,10 @@
 fn main() {
     let mut x: &str = "Sanskar";
     println!("My name is {x}");
-    // First le change the name in the variable by using the muttable and immutable.
-    // x= Coder
+    // First I change the name in the variable by using the muttable and immutable.
+    let x:&str = ("Coder");
     // or we can write this in the this way:-
-    // println!("I am the {}",x)
+    println!("I am the {}",x);
 }
 // ---------------------------------------------------------
 // See the next exercise for the more info.
