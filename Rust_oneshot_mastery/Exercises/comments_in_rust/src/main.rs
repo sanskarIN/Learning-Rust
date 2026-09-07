f3e1f2b9de3643e79d63275771cb04af632a8ab6
Sanskar <sanskarin@outlook.in>
2026-09-07 20:26:10 +0530

    feat(exercise-comments_in_rust): add source code

---
+++ Rust_oneshot_mastery/Exercises/comments_in_rust/src/main.rs
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------------
+// Hey everyone! In this exercise, we will learn about comments in Rust. Comments are an essential part of writing clean and understandable code. They allow developers to explain their code, making it easier for others (and themselves) to understand the logic and purpose behind it.
+// This is a comment in Rust. Comments are ignored by the compiler and are used to explain code to humans.
+// -----------------------------------------------------------------------------------
+/* This is a block comment in Rust. It can span multiple lines and is used to explain code to humans. */
+// -----------------------------------------------------------------------------------
+// This is a documentation comment (//!) in Rust. It is used to generate documentation for your code. Documentation comments are written using triple slashes (///) and can be used to describe functions, structs, enums, and other items in your code.
+// Documentation comments (//!) can also include examples, usage instructions, and other relevant information to help developers understand how to use your code effectively.
+// -----------------------------------------------------------------------------------
+fn main() {
+    // This is a single-line comment. It explains that the following line of code prints "Hello, world!" to the console.
+    println!("Hello, world!"); // This comment is placed at the end of a line of code.
+}
+// -----------------------------------------------------------------------------------
+// In this exercise, we have learned about different types of comments in Rust, including single-line comments, block comments, and documentation comments. Comments are an essential part of writing clean and understandable code, and they help developers explain their code to others (and themselves).
+// The code ends here. Remember to use comments wisely and effectively to make your code more readable and maintainable. Happy coding!
