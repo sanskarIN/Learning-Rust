9e429dc47214b16d66d2e540765c4a148d9c6d1b
Sanskar <sanskarin@outlook.in>
2026-09-07 20:27:15 +0530

    feat(exercise-view_address_of_variable): add source code

---
+++ Rust_oneshot_mastery/Exercises/view_address_of_variable/src/main.rs
@@ -0,0 +1,8 @@
+// Hello everyone I am going check the variable location via writing the code in the Rust.
+fn main() {
+    let x = 42;
+
+    println!("value = {}", x);
+    println!("address = {:p}", &x);
+}
+// The code ends here.
