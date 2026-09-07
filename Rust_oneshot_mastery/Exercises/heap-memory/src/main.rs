8b133506929a491860823d783ba049df9e49db14
Sanskar <sanskarin@outlook.in>
2026-09-07 20:26:15 +0530

    feat(exercise-heap-memory): add source code

---
+++ Rust_oneshot_mastery/Exercises/heap-memory/src/main.rs
@@ -0,0 +1,5 @@
+// Hey everyone in this file we are going to understand the concept of the Heap Memory in the Rust.
+fn main() {
+    let name = String::from("Rust");
+    println!("Before scope ends: {name}");
+} // deallocation happens here
